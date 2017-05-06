16ea7423f504587456a89e35c90c741e539dbbcf
nandu_prajapati <nantech19@gmail.com>
2017-05-07 00:01:34 +0100

    CheckIn0004: tabs creation with mine css

--- components/home/home.controller.js
+++ components/home/home.controller.js
@@ -7,5 +7,20 @@ var myApp = angular.module('nanduApp');
 myApp.controller('homeCtrl', ['$scope','$http', '$q', function ($scope, $http, $q) {
     $scope.greeting = 'Hola!';
 
+    $scope.tabs = [
+        { title:'Dynamic Title 1', content:'Dynamic content 1' },
+        { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+      ];
+
+      $scope.alertMe = function() {
+        setTimeout(function() {
+          $window.alert('You\'ve selected the alert tab!');
+        });
+      };
+
+      $scope.model = {
+        name: 'Tabs'
+      };
+
 
 }]);
